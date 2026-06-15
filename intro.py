ed86051fbd9b7e3801a2613c7d287bdfa6fa9ee6
angkonnath <145487293+angkon101@users.noreply.github.com>
2026-06-15 06:47:41 +0600

    Added new lines

--- intro.py
+++ intro.py
@@ -1,2 +1,8 @@
 print("Hello, World!")
 print("Welcome to Python programming.")
+# This is the simple python program. 
+# Using the # symbol, we can write single line comments in our code.
+"""
+This is a multi-line comment."""
+
+print("Hello"); print("World"); print("'Welcome in the intro'")
